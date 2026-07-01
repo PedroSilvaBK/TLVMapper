0d63f3a2ca12aa82ea99f5792289fdecfb52494f
Pedro Silva <pedro.rfsilva@ceiia.com>
2026-07-01 11:32:24 +0100

    Added helper methods to TLVUtils class

--- lib/src/main/java/io/github/pedrosilvabk/utils/TLVUtils.java
+++ lib/src/main/java/io/github/pedrosilvabk/utils/TLVUtils.java
@@ -1,5 +1,10 @@
 package io.github.pedrosilvabk.utils;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class TLVUtils {
     public static byte[] encodeLength(int arrLength) {
         if (arrLength < 0x80) {
@@ -85,5 +90,152 @@ public class TLVUtils {
             offset += arr.length;
         }
         return result;
+    }
+
+    public static Map<Integer, byte[]> findFUllTLVs(byte[] tlv, List<Integer> tags) {
+        if (tags.isEmpty()) {
+            return null;
+        }
+
+        Map<Integer, byte[]> result = new HashMap<>();
+        for (Integer tag : tags) {
+            result.put(tag, null);
+        }
+
+        int[] count = {0};
+
+        parseTLV(tlv, 0, tlv.length, result, count);
+
+        return result;
+    }
+
+    private static void parseTLV(byte[] tlv, int from, int to, Map<Integer, byte[]> result, int[] count) {
+        int index = 0;
+        int length = to - from;
+        while (index < length) {
+            if (count[0] == result.size()) {
+                break;
+            }
+
+            int[] tlvRead = readOneTLV(Arrays.copyOfRange(tlv, from, to), index);
+
+            int tag = tlvRead[0];
+            int valueIndex = tlvRead[2];
+            int nextIndex = tlvRead[tlvRead.length - 1];
+
+            if (result.containsKey(tag)) {
+                result.put(tag, Arrays.copyOfRange(tlv, from + index, from + nextIndex));
+                count[0]++;
+            }
+
+            index = nextIndex;
+
+            if (count[0] == result.size()) {
+                break;
+            }
+
+            int firstTagByte;
+            if (tag > 0xFFFF) {
+                firstTagByte = (tag >> 16) & 0xFF;
+            } else if (tag > 0xFF) {
+                firstTagByte = (tag >> 8) & 0xFF;
+            } else {
+                firstTagByte = tag & 0xFF;
+            }
+
+            boolean constructed = (firstTagByte & 0x20) != 0;
+
+            if (constructed) {
+                // This needs to happen because this function doesnt rely an known tag registry to work, which means that some times it can
+                // keep on recursing without valid results
+                // the only way to fix this is by having this class make use of the registry
+                try {
+                    parseTLV(tlv, from + valueIndex, from + nextIndex, result, count);
+                } catch (Exception e) {
+                    // Not actually constructed, skip
+                }
+            }
+        }
+    }
+
+    public static byte[] buildTlv(int tag, int expectedPayloadLength, byte[] payload) {
+        byte[] tagBytes = encodeTag( tag );
+        if (expectedPayloadLength != payload.length ) {
+            throw new IllegalArgumentException("Payload length does not match expected length");
+        }
+
+        byte[] lengthBytes = encodeLength(payload.length);
+        return concat(tagBytes, lengthBytes, payload);
+    }
+
+    public static byte[] buildTlv(int tag, int payload) {
+        byte[] tagBytes = encodeTag(tag);
+        if (payload == 0) {
+            return concat(tagBytes, encodeLength(payload));
+        } else {
+            byte[] payloadBytes = new byte[]{(byte)(payload & 0xFF)};
+            byte[] lengthBytes = encodeLength(payloadBytes.length);
+            return concat(tagBytes, lengthBytes, payloadBytes);
+        }
+    }
+
+    public static byte[] buildTlv(int tag, byte[] payload) {
+        byte[] tagBytes = encodeTag(tag);
+        byte[] lengthBytes = encodeLength(payload.length);
+        return concat(tagBytes, lengthBytes, payload);
+    }
+
+    private static int[] readOneTLV(byte[] tlv, int index) {
+        int firstByte = tlv[index++];
+        boolean isTwoByteTag = (firstByte & 0x1F) == 0x1F;
+
+        if (isTwoByteTag){
+            int secondByte = tlv[index++];
+
+            firstByte = (firstByte << 8) | (secondByte & 0xFF);
+        }
+
+        int length = 0;
+
+        int firstLengthByte = tlv[index];
+
+        if ((firstLengthByte & 0x80) == 0){
+            length = firstLengthByte;
+            index++;
+        }
+        else {
+            int numberOfBytesLength = tlv[index++] & 0x7F;
+
+            for (int i = 0; i < numberOfBytesLength; i++) {
+                length = (length << 8);
+
+                length = length | (tlv[index++] & 0xFF);
+            }
+        }
+
+        // mask the tag to be unsigned, here because of the registry keys
+        if (isTwoByteTag) {
+            firstByte = firstByte & 0xFFFF;
+        } else {
+            firstByte = firstByte & 0xFF;
+        }
+
+        int[] parsed = new int[] {
+                firstByte,
+                length,
+                index,
+                0,
+        };
+
+        parsed[parsed.length - 1] = index + length;
+
+        return parsed;
+    }
+
+    /// This extracts a tlv with tag
+    public static byte[] extractOneTLV(byte[] tlv, int index) {
+        int[] parsed = readOneTLV( tlv, index );
+
+        return Arrays.copyOfRange(tlv, index, parsed[parsed.length - 1]);
     }
 }
